07476fd982641c6d2d15f172c78bb0381b3823c7
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-14 12:28:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ class App < Sinatra::Base
 
 	get '/' do
 		erb :index
-	end 
-	
-    get "/info" do
-      "Testing the info page"
-    end
+	end
+
+  get "/info" do
+    "Testing the info page"
+  end
 end
